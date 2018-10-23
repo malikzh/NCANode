f5ef218d47b5e3cea91c46b2befcad5afbf48ae4
Malik Zharykoff <cmalikz.h@gmail.com>
2018-10-23 15:57:16 +0600

    fix bug with nullpointer exception

--- src/main/java/kz/ncanode/api/version/v10/methods/XMLVerify.java
+++ src/main/java/kz/ncanode/api/version/v10/methods/XMLVerify.java
@@ -81,9 +81,11 @@ public class XMLVerify extends ApiMethod {
 
                 chainInf = new ArrayList<>();
 
-                for (X509Certificate chainCert : chain) {
-                    JSONObject chi = man.pki.certInfo(chainCert, false, false, null);
-                    chainInf.add(chi);
+                if (chain != null) {
+                    for (X509Certificate chainCert : chain) {
+                        JSONObject chi = man.pki.certInfo(chainCert, false, false, null);
+                        chainInf.add(chi);
+                    }
                 }
             } catch (Exception e) {
                 throw new ApiErrorException(e.getMessage());
