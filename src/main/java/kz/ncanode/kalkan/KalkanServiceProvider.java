399e4ab3f3813c1e858631ddee497572547de87f
Anton Sergeyev <sergeyev@kolesa.kz>
2021-02-17 15:52:13 +0600

    подпись уже подписанного документа

--- src/main/java/kz/ncanode/kalkan/KalkanServiceProvider.java
+++ src/main/java/kz/ncanode/kalkan/KalkanServiceProvider.java
@@ -1,14 +1,24 @@
 package kz.ncanode.kalkan;
 
 import kz.gov.pki.kalkan.jce.provider.KalkanProvider;
+import kz.gov.pki.kalkan.jce.provider.cms.CMSException;
+import kz.gov.pki.kalkan.jce.provider.cms.CMSSignedData;
+import kz.gov.pki.kalkan.jce.provider.cms.SignerInformation;
+import kz.gov.pki.kalkan.jce.provider.cms.SignerInformationStore;
 import kz.gov.pki.kalkan.xmldsig.KncaXS;
 import kz.ncanode.ioc.ServiceProvider;
 import kz.ncanode.log.OutLogServiceProvider;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.varia.NullAppender;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.Provider;
 import java.security.Security;
+import java.security.cert.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Обёртка для загрузки провайдера Kalkan
@@ -34,5 +44,33 @@ public class KalkanServiceProvider implements ServiceProvider {
 
     public Provider get() {
         return provider;
+    }
+
+    /**
+     * Возвращает сертификаты, которым был подписан переданный документ
+     */
+    public List<X509Certificate> getCertificatesFromCmsSignedData(CMSSignedData cms) throws
+            NoSuchAlgorithmException,
+            NoSuchProviderException,
+            CMSException,
+            CertStoreException
+    {
+        List<X509Certificate> certs = new ArrayList<>();
+        SignerInformationStore signers = cms.getSignerInfos();
+        String providerName = this.provider.getName();
+        CertStore clientCerts = cms.getCertificatesAndCRLs("Collection", providerName);
+
+        for (Object signerObj : signers.getSigners()) {
+            SignerInformation signer = (SignerInformation) signerObj;
+            X509CertSelector signerConstraints = signer.getSID();
+            Collection<? extends Certificate> certCollection = clientCerts.getCertificates(signerConstraints);
+
+            for (Certificate certificate : certCollection) {
+                X509Certificate cert = (X509Certificate) certificate;
+                certs.add(cert);
+            }
+        }
+
+        return certs;
     }
 }
