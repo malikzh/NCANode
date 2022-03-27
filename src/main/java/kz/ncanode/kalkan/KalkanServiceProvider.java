86900f15b31feacd24a581abde4de5705c5d381b
zhasulan <zhasulan87@gmail.com>
2022-03-27 18:34:53 +0600

    fix idea recommended warnings kalkan

--- src/main/java/kz/ncanode/kalkan/KalkanServiceProvider.java
+++ src/main/java/kz/ncanode/kalkan/KalkanServiceProvider.java
@@ -24,18 +24,16 @@ import java.util.List;
  * Обёртка для загрузки провайдера Kalkan
  */
 public class KalkanServiceProvider implements ServiceProvider {
-    private KalkanProvider provider = null;
-    private OutLogServiceProvider out = null;
+    private final KalkanProvider provider;
 
     public KalkanServiceProvider(OutLogServiceProvider out) {
-        this.out = out;
 
-        this.out.write("Initializing Kalkan crypto...");
+        out.write("Initializing Kalkan crypto...");
         provider = new KalkanProvider();
         Security.addProvider(provider);
         BasicConfigurator.configure(new NullAppender());
         KncaXS.loadXMLSecurity();
-        this.out.write("Kalkan crypto initialized. Version: " + getVersion());
+        out.write("Kalkan crypto initialized. Version: " + getVersion());
     }
 
     public String getVersion() {
