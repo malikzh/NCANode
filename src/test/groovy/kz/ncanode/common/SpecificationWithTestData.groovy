b87385098cebf4be93d290ddfe6ac1b87fa16890
Malik Zharykov <cmalikz.h@gmail.com>
2022-08-20 18:18:38 +0600

    rename tests

--- src/test/groovy/kz/ncanode/common/SpecificationWithTestData.groovy
+++ src/test/groovy/kz/ncanode/common/SpecificationWithTestData.groovy
@@ -3,7 +3,7 @@ package kz.ncanode.common
 import kz.ncanode.constants.MessageConstants
 import spock.lang.Specification
 
-abstract class SpecificationWithKeys extends Specification {
+abstract class SpecificationWithTestData extends Specification {
 
     /**
      * Невалидный ключ, с невалидным Base64
