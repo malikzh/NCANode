f25ed7a04882b92f126c4bcf0186636bf4ce6c5c
Malik Zharykov <cmalikz.h@gmail.com>
2022-08-20 17:17:39 +0600

    add ncanode 3 banner

--- src/main/java/kz/ncanode/NCANode.java
+++ src/main/java/kz/ncanode/NCANode.java
@@ -6,6 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NCANode {
     public static void main(String[] args) {
+        System.out.println(banner());
         SpringApplication.run(NCANode.class, args);
+    }
+    private static String banner() {
+        return """
+             ____  _____   ______       _       ____  _____               __          ______  \s
+            |_   \\|_   _|.' ___  |     / \\     |_   \\|_   _|             |  ]        / ____ `.\s
+              |   \\ | | / .'   \\_|    / _ \\      |   \\ | |   .--.    .--.| | .---.   `'  __) |\s
+              | |\\ \\| | | |          / ___ \\     | |\\ \\| | / .'`\\ \\/ /'`\\' |/ /__\\\\  _  |__ '.\s
+             _| |_\\   |_\\ `.___.'\\ _/ /   \\ \\_  _| |_\\   |_| \\__. || \\__/  || \\__., | \\____) |\s
+            |_____|\\____|`.____ .'|____| |____||_____|\\____|'.__.'  '.__.;__]'.__.'  \\______.'\s
+            """;
     }
 }
