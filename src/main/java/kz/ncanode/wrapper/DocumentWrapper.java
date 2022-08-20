d5c3c1ef2f3e2ab9eafbf908eab943e562d85775
Malik Zharykov <cmalikz.h@gmail.com>
2022-08-20 18:03:26 +0600

    add xmldsig and wsse to wrapper

--- src/main/java/kz/ncanode/wrapper/DocumentWrapper.java
+++ src/main/java/kz/ncanode/wrapper/DocumentWrapper.java
@@ -4,14 +4,24 @@ import kz.ncanode.exception.ServerException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.xml.security.c14n.Canonicalizer;
+import org.apache.xml.security.encryption.XMLCipherParameters;
+import org.apache.xml.security.exceptions.XMLSecurityException;
+import org.apache.xml.security.transforms.Transforms;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
@@ -35,6 +45,55 @@ public class DocumentWrapper {
         } catch (ParserConfigurationException | IOException | SAXException e) {
             log.error("XML parsing error", e);
             throw new ServerException("Cannot read XML", e);
+        }
+    }
+
+    /**
+     * Создает и добавляет XMLDSIG подпись
+     * @return XML Signature Wrapper
+     */
+    public XMLSignatureWrapper createXmlSignature(CertificateWrapper certificateWrapper) {
+        final XMLSignatureWrapper sig = new XMLSignatureWrapper(getDocument(), certificateWrapper.getSignAlgorithmId());
+        getDocument().getFirstChild().appendChild(sig.getXmlSignature().getElement());
+        Transforms transforms = new Transforms(getDocument());
+
+        try {
+            transforms.addTransform(Transforms.TRANSFORM_ENVELOPED_SIGNATURE);
+            transforms.addTransform(XMLCipherParameters.N14C_XML_CMMNTS);
+            sig.getXmlSignature().addDocument("", transforms, certificateWrapper.getHashAlgorithmId());
+            sig.getXmlSignature().addKeyInfo(certificateWrapper.getX509Certificate());
+        } catch (XMLSecurityException e) {
+            log.error("XMLDSig Signature creation error", e);
+            throw new ServerException("XMLDSig Signature creation error", e);
+        }
+
+        return sig;
+    }
+
+    /**
+     * Создает и добавляет подпись WSSE
+     *
+     * @param certificateWrapper Сертификат
+     * @return
+     */
+    public XMLSignatureWrapper createWsseSignature(CertificateWrapper certificateWrapper) {
+        final XMLSignatureWrapper sig = new XMLSignatureWrapper(getDocument(),
+            certificateWrapper.getSignAlgorithmId(), Canonicalizer.ALGO_ID_C14N_EXCL_OMIT_COMMENTS);
+
+        getDocument().getFirstChild().appendChild(sig.getXmlSignature().getElement());
+
+        return sig;
+    }
+
+    @Override
+    public String toString() {
+        try (StringWriter stringWriter = new StringWriter()) {
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.transform(new DOMSource(getDocument()), new StreamResult(stringWriter));
+            return stringWriter.toString();
+        } catch (IOException | TransformerException e) {
+            log.error("XML generation error", e);
+            throw new ServerException("XML generation error", e);
         }
     }
 }
--- src/main/java/kz/ncanode/wrapper/XMLSignatureWrapper.java
+++ src/main/java/kz/ncanode/wrapper/XMLSignatureWrapper.java
@@ -2,22 +2,51 @@ package kz.ncanode.wrapper;
 
 import kz.ncanode.exception.ServerException;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.xml.security.exceptions.XMLSecurityException;
 import org.apache.xml.security.signature.XMLSignature;
+import org.apache.xml.security.signature.XMLSignatureException;
+import org.apache.xml.security.utils.XMLUtils;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import java.security.PrivateKey;
 
+@Slf4j
 public class XMLSignatureWrapper {
 
     @Getter
     private final XMLSignature xmlSignature;
 
-    public XMLSignatureWrapper(Document xml, CertificateWrapper certificate, PrivateKey privateKey) {
+    public XMLSignatureWrapper(Document document, String signAlgorithmId) {
         try {
-            xmlSignature = new XMLSignature(xml, "", certificate.getSignAlgorithmId());
+            xmlSignature = new XMLSignature(document, "", signAlgorithmId);
         } catch (XMLSecurityException e) {
             throw new ServerException("XML Signature creation error", e);
+        }
+    }
+
+    public XMLSignatureWrapper(Document document, String signAlgorithmId, String c14nAlgorithmId) {
+        try {
+            Element signatureMethod = XMLUtils.createElementInSignatureSpace(document, "SignatureMethod");
+            signatureMethod.setAttributeNS(null, "Algorithm", signAlgorithmId);
+
+            Element c14nMethod = XMLUtils.createElementInSignatureSpace(document, "CanonicalizationMethod");
+            c14nMethod.setAttributeNS(null, "Algorithm", c14nAlgorithmId);
+
+            xmlSignature = new XMLSignature(document, "", signatureMethod, c14nMethod);
+        } catch (XMLSecurityException e) {
+            log.error("XML Signature creation error", e);
+            throw new ServerException("XML Signature creation error", e);
+        }
+    }
+
+    public void sign(PrivateKey privateKey) {
+        try {
+            getXmlSignature().sign(privateKey);
+        } catch (XMLSignatureException e) {
+            log.error("XML Signing error", e);
+            throw new ServerException("XML Signing error", e);
         }
     }
 }
