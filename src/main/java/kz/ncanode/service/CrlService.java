0049876dcd2f590f52fb8a842b58c569cba293e5
Malik Zharykov <cmalikz.h@gmail.com>
2022-08-21 19:47:16 +0600

    test downloading

--- src/main/java/kz/ncanode/service/CrlService.java
+++ src/main/java/kz/ncanode/service/CrlService.java
@@ -4,9 +4,16 @@ import kz.ncanode.configuration.properties.CrlConfigurationProperties;
 import kz.ncanode.configuration.properties.SystemConfigurationProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
@@ -22,6 +29,7 @@ public class CrlService {
 
     private final SystemConfigurationProperties systemConfigurationProperties;
     private final CrlConfigurationProperties crlConfigurationProperties;
+    private final CloseableHttpClient client;
 
     @Scheduled(fixedRateString = "${ncanode.crl.ttl}", initialDelay = 0, timeUnit = TimeUnit.MINUTES)
     public void updateCache() {
@@ -31,11 +39,36 @@ public class CrlService {
 
         log.info("Updating CRL cache...");
 
-
     }
 
-    public void download(String url, Path path) {
+    /**
+     * Скачивает CRL файл
+     *
+     * @param url
+     * @param path
+     */
+    private void download(String url, Path path) {
+        try(CloseableHttpResponse response = client.execute(new HttpGet(url))) {
+            int status = response.getStatusLine().getStatusCode();
 
+            if (status != HttpStatus.OK.value()) {
+                log.error("Cannot download file: {}", url);
+                return;
+            }
+
+            HttpEntity entity = response.getEntity();
+
+            if (entity == null) {
+                log.error("Got empty request: {}", url);
+                return;
+            }
+
+            try(FileOutputStream out = new FileOutputStream(path.toFile())) {
+                entity.writeTo(out);
+            }
+        } catch (IOException e) {
+            log.error("Cannot download file from url: {}", url, e);
+        }
     }
 
     private Path getCrlCacheFilename(String fileName) {
