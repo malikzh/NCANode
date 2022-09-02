8ec88409e29358c6ca4c1b711e65b872c5214ca3
Malik Zharykov <cmalikz.h@gmail.com>
2022-09-02 18:17:33 +0600

    create crl

--- src/main/java/kz/ncanode/configuration/properties/CrlConfigurationProperties.java
+++ src/main/java/kz/ncanode/configuration/properties/CrlConfigurationProperties.java
@@ -8,11 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import java.net.URL;
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Objects;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Configuration
@@ -25,12 +21,6 @@ public class CrlConfigurationProperties {
     private String url;
 
     public Map<String, URL> getUrlList() {
-        return Arrays.stream(url.split("\\s+"))
-            .map(Util::createNewUrl)
-            .filter(Objects::nonNull)
-            .collect(Collectors.toMap(
-                url -> Util.sha1(url.toString()),
-                Function.identity()
-            ));
+        return Util.urlMap(getUrl());
     }
 }
