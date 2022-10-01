cd4a74ba3c377612344691c8c96e97fbe374deac
Malik Zharykov <cmalikz.h@gmail.com>
2022-10-01 14:44:24 +0600

    change response in pkcs12 info

--- src/main/java/kz/ncanode/controller/Pkcs12Controller.java
+++ src/main/java/kz/ncanode/controller/Pkcs12Controller.java
@@ -1,13 +1,9 @@
 package kz.ncanode.controller;
 
-import kz.ncanode.dto.certificate.CertificateRevocation;
 import kz.ncanode.dto.request.Pkcs12InfoRequest;
-import kz.ncanode.dto.response.Pkcs12InfoResponse;
+import kz.ncanode.dto.response.VerificationResponse;
 import kz.ncanode.service.CertificateService;
-import kz.ncanode.wrapper.KalkanWrapper;
-import kz.ncanode.wrapper.KeyStoreWrapper;
 import lombok.RequiredArgsConstructor;
-import lombok.val;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,29 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("pkcs12")
 @RequiredArgsConstructor
 public class Pkcs12Controller {
-
-    private final KalkanWrapper kalkanWrapper;
     private final CertificateService certificateService;
 
     @PostMapping("/info")
-    public ResponseEntity<Pkcs12InfoResponse> info(@Valid @RequestBody Pkcs12InfoRequest request) {
-        return ResponseEntity.ok(Pkcs12InfoResponse.builder()
-            .certificates(Optional.of(request.getKeys()).map(kalkanWrapper::read).orElseThrow().stream()
-                .map(KeyStoreWrapper::getCertificate).map(cert -> {
-                    val date = certificateService.getCurrentDate();
-                    val withOcsp = request.getRevocationCheck().contains(CertificateRevocation.OCSP);
-                    val withCrl = request.getRevocationCheck().contains(CertificateRevocation.CRL);
-
-                    certificateService.attachValidationData(cert, withOcsp, withCrl);
-
-                    return cert.toCertificateInfo(date, withOcsp, withCrl);
-                }).toList())
-            .build());
+    public ResponseEntity<VerificationResponse> info(@Valid @RequestBody Pkcs12InfoRequest request) {
+        return ResponseEntity.ok(certificateService.verifyCerts(request));
     }
 }
--- src/main/java/kz/ncanode/service/CertificateService.java
+++ src/main/java/kz/ncanode/service/CertificateService.java
@@ -1,10 +1,18 @@
 package kz.ncanode.service;
 
+import kz.ncanode.dto.certificate.CertificateInfo;
+import kz.ncanode.dto.certificate.CertificateRevocation;
+import kz.ncanode.dto.request.Pkcs12InfoRequest;
+import kz.ncanode.dto.response.VerificationResponse;
 import kz.ncanode.wrapper.CertificateWrapper;
+import kz.ncanode.wrapper.KalkanWrapper;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +20,7 @@ public class CertificateService {
     public final CrlService crlService;
     public final OcspService ocspService;
     public final CaService caService;
+    public final KalkanWrapper kalkanWrapper;
 
     public void attachValidationData(final CertificateWrapper cert, boolean checkOcsp, boolean checkCrl) {
         cert.setIssuerCertificate(caService.getRootCertificateFor(cert).orElseThrow());
@@ -21,5 +30,32 @@ public class CertificateService {
 
     public Date getCurrentDate() {
         return new Date();
+    }
+
+    public VerificationResponse verifyCerts(Pkcs12InfoRequest request) {
+        var valid = true;
+        val date = getCurrentDate();
+        val withOcsp = request.getRevocationCheck().contains(CertificateRevocation.OCSP);
+        val withCrl = request.getRevocationCheck().contains(CertificateRevocation.CRL);
+
+        val keys = Optional.of(request.getKeys()).map(kalkanWrapper::read).orElseThrow();
+        val certs = new ArrayList<CertificateInfo>();
+
+        for (var key : keys) {
+            val cert = key.getCertificate();
+
+            attachValidationData(cert, withOcsp, withCrl);
+
+            if (!cert.isValid(date, withOcsp, withCrl)) {
+                valid = false;
+            }
+
+            certs.add(cert.toCertificateInfo(date, withOcsp, withCrl));
+        }
+
+        return VerificationResponse.builder()
+            .valid(valid)
+            .signers(certs)
+            .build();
     }
 }
