c992288a749e740f366b08879d399bb719472541
Malik Zharykov <cmalikz.h@gmail.com>
2022-10-01 21:19:09 +0600

    add negative testcase for keystore

--- src/test/groovy/kz/ncanode/unit/wrapper/KeyStoreWrapperTest.groovy
+++ src/test/groovy/kz/ncanode/unit/wrapper/KeyStoreWrapperTest.groovy
@@ -1,13 +1,22 @@
 package kz.ncanode.unit.wrapper
 
 import kz.ncanode.common.WithTestData
-
+import kz.ncanode.exception.ServerException
 import kz.ncanode.wrapper.KalkanWrapper
 import kz.ncanode.wrapper.KeyStoreWrapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
 import spock.lang.Unroll
+
+import java.security.KeyStore
+import java.security.KeyStoreSpi
+import java.security.NoSuchAlgorithmException
+import java.security.UnrecoverableKeyException
+
+import static org.mockito.ArgumentMatchers.any
+import static org.mockito.Mockito.doThrow
+import static org.mockito.Mockito.mock
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class KeyStoreWrapperTest extends Specification implements WithTestData {
@@ -49,6 +58,29 @@ class KeyStoreWrapperTest extends Specification implements WithTestData {
         caseName            | signerId
         'test for 2015 key' | 0
         'test for 2004 key' | 1
+    }
+
+    @Unroll("#caseName")
+    def "check exception thrown for getPrivateKey"() {
+        given:
+        def keyStoreSpiMock = mock(KeyStoreSpi.class);
+        def keystore = new KeyStore(keyStoreSpiMock, null, "test"){ };
+        keystore.load(null)
+
+        doThrow(exception).when(keyStoreSpiMock).engineGetKey(any(), any())
+
+        def keystoreWrapper = new KeyStoreWrapper(keystore, "null", "")
+
+        when:
+        keystoreWrapper.getPrivateKey()
+
+        then:
+        thrown(ServerException)
+
+        where:
+        caseName                   | exception
+        'NoSuchAlgorithmException' | new NoSuchAlgorithmException()
+        'UnrecoverableKeyException' | new UnrecoverableKeyException()
     }
 
     private KeyStoreWrapper createKeyStore2015() {
