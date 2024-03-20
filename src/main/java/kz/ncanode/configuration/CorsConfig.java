970f43930cfdd906e056ae516ef065cfd0d634ba
Zhunisali <zshanabek@gmail.com>
2024-03-20 12:15:55 +0500

    Update package name in CorsConfig.java

--- src/main/java/kz/ncanode/configuration/CorsConfig.java
+++ src/main/java/kz/ncanode/configuration/CorsConfig.java
@@ -1,4 +1,4 @@
-package kz.arcana.pledgeregistration.config;
+package kz.ncanode.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
