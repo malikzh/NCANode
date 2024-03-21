4761c3c7d3932451294b790c775a946dcda045e4
Zhunisali <zshanabek@gmail.com>
2024-03-21 13:05:23 +0500

    Update allowed origins in CorsConfig.java

--- src/main/java/kz/ncanode/configuration/CorsConfig.java
+++ src/main/java/kz/ncanode/configuration/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
