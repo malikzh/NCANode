cbc84ea3a42eef04d6b1d076a9fb344041eded54
Zhunisali <zshanabek@gmail.com>
2024-03-22 05:32:36 +0500

    commit

--- src/main/java/kz/ncanode/configuration/CorsConfiguration.java
+++ src/main/java/kz/ncanode/configuration/CorsConfiguration.java
@@ -6,13 +6,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfiguration {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                System.out.println("CORS CONFIGURED");
                 registry.addMapping("/**")
                         .allowedOrigins("https://magnum-ashen.vercel.app", "http://localhost:3000", "https://cors-test.codehappy.dev", "http://89.46.34.50")
                         .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS") 
