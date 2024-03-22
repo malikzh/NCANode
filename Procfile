3cbdeb40584969a68e238ccb136256108300fa62
Zhunisali <zshanabek@gmail.com>
2024-03-22 13:28:32 +0500

    Update Procfile and NCANode.java, delete MainController.java

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: ./gradlew bootJar
 web: ./gradlew bootRun
--- src/main/java/kz/ncanode/NCANode.java
+++ src/main/java/kz/ncanode/NCANode.java
@@ -7,26 +7,39 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
 @EnableRetry
+@Controller
+@SpringBootApplication
 public class NCANode extends SpringBootServletInitializer {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
     public static void main(String[] args) {
         System.out.println(banner());
         SpringApplication.run(NCANode.class, args);
     }
+
     private static String banner() {
         return """
-             ____  _____   ______       _       ____  _____               __          ______  \s
-            |_   \\|_   _|.' ___  |     / \\     |_   \\|_   _|             |  ]        / ____ `.\s
-              |   \\ | | / .'   \\_|    / _ \\      |   \\ | |   .--.    .--.| | .---.   `'  __) |\s
-              | |\\ \\| | | |          / ___ \\     | |\\ \\| | / .'`\\ \\/ /'`\\' |/ /__\\\\  _  |__ '.\s
-             _| |_\\   |_\\ `.___.'\\ _/ /   \\ \\_  _| |_\\   |_| \\__. || \\__/  || \\__., | \\____) |\s
-            |_____|\\____|`.____ .'|____| |____||_____|\\____|'.__.'  '.__.;__]'.__.'  \\______.'\s
-            """;
+                 ____  _____   ______       _       ____  _____               __          ______  \s
+                |_   \\|_   _|.' ___  |     / \\     |_   \\|_   _|             |  ]        / ____ `.\s
+                  |   \\ | | / .'   \\_|    / _ \\      |   \\ | |   .--.    .--.| | .---.   `'  __) |\s
+                  | |\\ \\| | | |          / ___ \\     | |\\ \\| | / .'`\\ \\/ /'`\\' |/ /__\\\\  _  |__ '.\s
+                 _| |_\\   |_\\ `.___.'\\ _/ /   \\ \\_  _| |_\\   |_| \\__. || \\__/  || \\__., | \\____) |\s
+                |_____|\\____|`.____ .'|____| |____||_____|\\____|'.__.'  '.__.;__]'.__.'  \\______.'\s
+                """;
     }
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(NCANode.class);
