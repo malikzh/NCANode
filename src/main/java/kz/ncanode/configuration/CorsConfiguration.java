4c02a25e367eef0d88a5897151e1cb82cb5a505e
Malik Zharykov <cmalikz.h@gmail.com>
2024-08-23 07:11:21 +0500

    feat(cors): disable cors

---
+++ src/main/java/kz/ncanode/configuration/CorsConfiguration.java
@@ -0,0 +1,13 @@
+package kz.ncanode.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
